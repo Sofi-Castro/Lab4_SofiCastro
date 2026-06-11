e8f1cc0707f38c252c9846202176ce8d51ba999a
Sofi-Castro <castrosalassofia@gmail.com>
2026-06-11 08:27:57 -0600

    Corrección de errores con tipo de dato y lógica en el main para guardar una copia del arreglo original

--- ejercicio2.c
+++ ejercicio2.c
@@ -3,7 +3,7 @@
 
 unsigned char *read_pgm(const char *filename, int *width, int *height, int *max_val){
     
-    FILE *fp = fopen(*filename, "r");
+    FILE *fp = fopen(filename, "r");
 
     if (fp == NULL) {
         return NULL;
@@ -64,17 +64,19 @@ unsigned char *make_negative(unsigned char *pixels, int total){
 
 void write_pgm(const char *filename, unsigned char *pixels, int width, int height, int max_val){
 
-     FILE *fp = fopen(*filename, "w");
+    FILE *fp = fopen(filename, "w");
 
     fprintf(fp, "P2\n");
-    fprint(fp, "%d %d\n", width, height);
-    fprint(fp, "%d\n", max_val);
+    fprintf(fp, "%d %d\n", width, height);
+    fprintf(fp, "%d\n", max_val);
 
      for (int i = 0; i < (width * height); i++) {
         fprintf(fp, "%u ", *pixels);
 
+        pixels++;
+
         if ((i+1) % width == 0){
-            fprint(fp, "\n");
+            fprintf(fp, "\n");
         }
 
      }
@@ -85,8 +87,8 @@ void write_pgm(const char *filename, unsigned char *pixels, int width, int heigh
 
 void print_stats(unsigned char *original, unsigned char *thresholded, int total){
 
-    unsigned char blanco = 0, negro = 0, suma = 0;
-    int promedio = 0;
+    unsigned char blanco = 0, negro = 0;
+    int promedio = 0, suma = 0;
 
     for (int i = 0; i < total; i++){
 
@@ -96,31 +98,26 @@ void print_stats(unsigned char *original, unsigned char *thresholded, int total)
             negro += 1;
         }
 
-        suma += *original;
+        suma += (int)*original;
 
         original++;
         thresholded++;
 
     }
 
-    promedio = (int)suma / total;
+    promedio = suma / total;
 
-
-    printf("La cantidad de pixeles blancos es %hhu y la cantidad de pixeles negros es %huu\n", blanco, negro);
+    printf("La cantidad de pixeles blancos es %hhu y la cantidad de pixeles negros es %hhu\n", blanco, negro);
     printf("El promedio de los pixeles del arreglo original es %d\n", promedio);
 
 }
 
-void freearray(int *pixels){
-    free (pixels);
-}
-
 int main(void){
+
     int width, height, max_val, threshold, total;
     unsigned char *pixels = NULL;
     unsigned char *negative = NULL;
-    unsigned char *original = NULL;
-    char *ruta = "imagen.pgm";
+    char *ruta = "input.pgm";
     char *archivo_1 = "output_threshold.pgm";
     char *archivo_2 = "output_negative.pgm";
 
@@ -128,22 +125,35 @@ int main(void){
     scanf("%d", &threshold);
 
     pixels = read_pgm(ruta, &width, &height, &max_val);
+    unsigned char *p1 = pixels;
 
+    if (pixels == NULL) {
+        printf("Error al leer la imagen\n");
+        return 0;
+    }
+    
     total = width * height;
 
+    unsigned char *original = malloc(total * sizeof(unsigned char));
+    unsigned char *p2 = original;
+
+    for (int i = 0; i < total; i++) {
+        *p2 = *p1;
+        p1++;
+        p2++;
+    }
+
     apply_threshold(pixels, total, threshold);
-    negative = make_negative(pixels, width*height);
+    negative = make_negative(pixels, total);
 
     write_pgm(archivo_1, pixels, width, height, max_val);
     write_pgm(archivo_2, negative, width, height, max_val);
 
-    original = read_pgm(ruta, &width, &height, &max_val);
-
     print_stats(original, pixels, total);
 
-    freearray(original);
-    freearray(negative);
-    freearray(pixels);
-
+    free(negative);
+    free(pixels);
+    free(original);
+    
     return 0;
 }
